a4c73e4714fbf68be9fd6d498383e69629d7db0c
mehran <iomehran@riseup.net>
2021-08-28 00:02:53 +0430

    feat: add allowed email domains to config

--- src/config.py
+++ src/config.py
@@ -9,13 +9,10 @@ SQLITE_DB_FILE_EXTENSION = '.db'
 
 KARSU_SUBMIT_ENDPOINT = 'https://panel.karsu.ir/api/InputOutput/add-input-output'
 
-
-# you must fill this dict with key `email` and value `token`, which token refers to karsu logged in jwt token
-EMAILS_TOKENS: dict = {
-}
-
-
 START_WORK_TIME = '10:00'
 END_WORK_TIME = '18:30'
 
 WORKSPACE_CODE = 2219
+
+ALLOWED_EMAIL_DOMAINS = (
+)
