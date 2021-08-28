0512c8b3963ea2c549de81a7c84bc0d3d4e45d0d
mehran <iomehran@riseup.net>
2021-08-28 11:45:52 +0430

    feat(data): add last submit field to token model

--- data/models.py
+++ data/models.py
@@ -1,16 +1,21 @@
+from datetime import datetime
+
+
 class Token:
     id: int
     email: str
     value: str
+    last_submit: datetime
 
-    def __init__(self, id: int, email: str, value: str):
+    def __init__(self, id: int, email: str, value: str, last_submit: datetime):
         self.id = id
         self.email = email
         self.value = value
+        self.last_submit = last_submit
 
     @staticmethod
     def from_db_cursor(cursor):
-        return Token(cursor[0], cursor[1], cursor[2])
+        return Token(cursor[0], cursor[1], cursor[2], cursor[3])
 
     def __str__(self):
         return f'token for: {self.email}'
